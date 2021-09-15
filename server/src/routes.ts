4b0d99923aa95d36dd90d4aa9f2ad9ef5ac91df0
wanessaramos <wanessinha_ramos@yahoo.com.br>
2021-09-15 09:34:17 -0300

    feat: adiciona try-catch na rota users

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -19,26 +19,34 @@ routes.post("/users", async (request, response) => {
 
   const trx = await db.transaction();
 
-  const insertedDistrictIds = await trx("district").insert({
-    cep,
-  });
+  try {
+    const insertedDistrictIds = await trx("district").insert({
+      cep,
+    });
 
-  const district_id = insertedDistrictIds[0];
+    const district_id = insertedDistrictIds[0];
 
-  insertedUsersIds = await trx("users").insert({
-    name,
-    picture,
-    cpf,
-    phone,
-    login,
-    password,
-    email,
-    district_id,
-  });
+    insertedUsersIds = await trx("users").insert({
+      name,
+      picture,
+      cpf,
+      phone,
+      login,
+      password,
+      email,
+      district_id,
+    });
 
-  await trx.commit();
+    await trx.commit();
 
-  return response.send();
+    return response.status(201).send();
+  } catch (err) {
+    await trx.rollback();
+
+    return response.status(400).json({
+      error: "Erro inesperado ao criar um usuário",
+    });
+  }
 });
 
 routes.post("/waste", async (request, response) => {
