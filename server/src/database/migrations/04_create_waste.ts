25ad6bd6efc4ca4e8097fa261cfac8d77e91dcce
kdu <kdu.eduardo97@gmail.com>
2021-09-22 19:05:43 -0300

    fix: adiciona atributo quantity e date

--- server/src/database/migrations/04_create_waste.ts
+++ server/src/database/migrations/04_create_waste.ts
@@ -4,21 +4,23 @@ export async function up(knex: Knex) {
   return knex.schema.createTable("waste", (table) => {
     table.increments("id").primary();
     table.string("name").notNullable();
+    table.string("date").notNullable();
     table.string("picture").notNullable();
+    table.decimal("quantity").notNullable();
     table.string("description").notNullable();
 
     table
-      .integer("types_id")
+      .integer("waste_types_id")
       .notNullable()
       .references("id")
-      .inTable("types")
+      .inTable("waste_types")
       .onUpdate("CASCADE");
 
     table
-      .integer("amounts_id")
+      .integer("measurement_unit_id")
       .notNullable()
       .references("id")
-      .inTable("amounts")
+      .inTable("measurement_unit")
       .onUpdate("CASCADE");
   });
 }
